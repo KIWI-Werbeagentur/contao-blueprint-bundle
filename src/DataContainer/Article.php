76c8f75d7a9a14b383fa6bece088661fa6cf1253
Kate <kzimmermann@kiwi.de>
2024-11-20 17:20:12 +0100

    remove unnecessary code

--- src/DataContainer/Article.php
+++ src/DataContainer/Article.php
@@ -66,34 +66,6 @@ class Article
             'table' => $strTable,
             'mode' => $strTable == 'tl_article' ? 1 : 2
         ]);
-
-        $security = System::getContainer()->get('security.helper');
-
-        $id = $arrData['id'];
-
-        $labelPasteAfter = $GLOBALS['TL_LANG'][$strTable]['pasteafter'] ?? $GLOBALS['TL_LANG']['DCA']['pasteafter'];
-        $imagePasteAfter = Image::getHtml('pasteafter.svg', \sprintf($labelPasteAfter[1], $id));
-
-        $labelPasteInto = $GLOBALS['TL_LANG'][$strTable]['pasteinto'] ?? $GLOBALS['TL_LANG']['DCA']['pasteinto'];
-        $imagePasteInto = Image::getHtml('pasteinto.svg', \sprintf($labelPasteInto[1], $id));
-
-        if ($strTable == "tl_article") {
-            if (($arrClipboard['mode'] == 'cut' && ($isCircular || $arrClipboard['id'] == $id)) || ($arrClipboard['mode'] == 'cutAll' && ($isCircular || \in_array($id, $arrClipboard['id']))) || !$this->canPasteClipboard($arrClipboard, ['pid' => $currentRecord['pid'], 'sorting' => $currentRecord['sorting'] + 1])) {
-                return Image::getHtml('pasteafter--disabled.svg') . ' ';
-            } else {
-                return '<a href="' . Backend::addToUrl('act=' . $arrClipboard['mode'] . '&amp;mode=1&amp;pid=' . $id . (!\is_array($arrClipboard['id']) ? '&amp;id=' . $arrClipboard['id'] : '')) . '" title="' . StringUtil::specialchars(\sprintf($labelPasteAfter[1], $id)) . '" data-action="contao--scroll-offset#store">' . $imagePasteAfter . '</a> ';
-            }
-        } else {
-            if (!$security->isGranted(ContaoCorePermissions::DC_PREFIX . 'tl_article', new CreateAction('tl_article', ['pid' => $arrData['id'], 'sorting' => $arrData['sorting']]))) {
-                return Image::getHtml('pasteinto--disabled.svg') . ' ';
-            } else {
-                System::loadLanguageFile('tl_article');
-                System::loadLanguageFile('default');
-                $labelPasteInto = $GLOBALS['TL_LANG']['tl_article']['pasteinto'] ?? $GLOBALS['TL_LANG']['DCA']['pasteinto'];
-                $imagePasteInto = Image::getHtml('pasteinto.svg', $labelPasteInto[0] ?? "");
-                return '<a href="' . Backend::addToUrl('act=' . $arrClipboard['mode'] . '&amp;mode=2&amp;pid=0' . (!\is_array($arrClipboard['id']) ? '&amp;id=' . $arrClipboard['id'] : '')) . '" title="' . StringUtil::specialchars($labelPasteInto[0] ?? "") . '" data-action="contao--scroll-offset#store">' . $imagePasteInto . '</a> ';
-            }
-        }
     }
 
     /*
