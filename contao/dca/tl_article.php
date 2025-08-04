6a4310967bdbccaf77bbf641e6d4273894a5ee81
Kate <kzimmermann@kiwi.de>
2025-08-04 15:47:28 +0200

    adjust DC_Table_Blueprint to allow clipboard pasting from different tables than tl_article

--- contao/dca/tl_article.php
+++ contao/dca/tl_article.php
@@ -3,6 +3,7 @@
 use \Contao\CoreBundle\DataContainer\PaletteManipulator;
 use Contao\System;
 use Kiwi\Contao\BlueprintsBundle\DataContainer\Article;
+use Kiwi\Contao\BlueprintsBundle\Drivers\DC_Table_Blueprint;
 
 $GLOBALS['TL_DCA']['tl_article']['list']['global_operations']['blueprint'] = [
     'href' => 'key=blueprint_article_insert&mode=create',
@@ -18,3 +19,5 @@ $GLOBALS['TL_DCA']['tl_article']['fields']['template'] = [
 $GLOBALS['TL_DCA']['tl_article']['config']['onload_callback'][] = [Article::class, 'initPasting'];
 
 $GLOBALS['TL_DCA']['tl_content']['config']['oncopy_callback'][] = [Article::class, 'onCopyListener'];
+
+$GLOBALS['TL_DCA']['tl_article']['config']['dataContainer'] = DC_Table_Blueprint::class;
--- src/Drivers/DC_Table_Blueprint.php
+++ src/Drivers/DC_Table_Blueprint.php
@@ -3,6 +3,8 @@
 namespace Kiwi\Contao\BlueprintsBundle\Drivers;
 
 use Contao\ArticleModel;
+use Contao\Backend;
+use Contao\Database;
 use Contao\System;
 use Kiwi\Contao\BlueprintsBundle\Model\BlueprintArticleModel;
 use Contao\DC_Table;
@@ -17,12 +19,62 @@ class DC_Table_Blueprint extends DC_Table
      * */
     public function getCurrentRecord(int|string|null $id = null, string|null $table = null): array|null
     {
-        if (Input::get('key') == 'blueprint_article_insert') {
+        if ((Input::get('act') == 'copy' || Input::get('act') == 'paste') && Input::get('key') == 'blueprint_article_clipboard') {
+            //Input::setGet('mode','create');
+            return (BlueprintArticleModel::findById(Input::get('id')))->row();
+        }
+        elseif (Input::get('key') == 'blueprint_article_insert') {
             return (BlueprintArticleModel::findById(Input::get('id')))->row();
         } elseif (Input::get('key') == 'article_insert') {
             return (ArticleModel::findById($this->intCurrentRecord))->row();
         }
         return parent::getCurrentRecord($id, $table);
+    }
+    public function showAll()
+    {
+        $this->limit = '';
+
+        $objSession = System::getContainer()->get('request_stack')->getSession();
+
+        $this->reviseTable();
+
+        // Add to clipboard
+        if (Input::get('act') == 'paste')
+        {
+            //$this->denyAccessUnlessGranted(...$this->getClipboardPermission(Input::get('mode'), (int) Input::get('id')));
+
+            $children = Input::get('children');
+
+            // Backwards compatibility
+            if (Input::get('childs') !== null)
+            {
+                trigger_deprecation('contao/core-bundle', '5.3', 'Using the "childs" query parameter has been deprecated and will no longer work in Contao 6. Use the "children" parameter instead.');
+                $children = Input::get('childs');
+            }
+
+            $arrClipboard = $objSession->get('CLIPBOARD');
+
+            $arrClipboard[$this->strTable] = array
+            (
+                'id' => Input::get('id'),
+                'childs' => $children, // backwards compatibility
+                'children' => $children,
+                'mode' => Input::get('mode')
+            );
+
+            $objSession->set('CLIPBOARD', $arrClipboard);
+
+            if ($this->currentPid)
+            {
+                $this->redirect(Backend::addToUrl('id=' . $this->currentPid, false, array('act', 'mode')));
+            }
+            else
+            {
+                $this->redirect(Backend::addToUrl('', false, array('act', 'mode', 'id')));
+            }
+        }
+
+        return parent::showAll();
     }
 
     /*
